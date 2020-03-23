baf47c5e9a7dae887bb28c09220002042167a8ba
Rachel Selby <rachel.b.selby-1@ou.edu>
2020-03-23 18:42:08 -0500

    remove direct pixel comparison

--- final.py
+++ final.py
@@ -48,62 +48,6 @@ def convertToImage(basepath, save_path, pdf):
     for page in pages:   
         page.save(new_path, 'JPEG')
 
-def comparePixels(px1, px2, num):
-    same = True
-    for i in range(3):
-        #print(px1[i], px2[i])
-        #print((px1[i] - px2[i]))
-        if(abs(px1[i] - px2[i]) > num):
-            same = False
-    return same
-
-def compareImages(basepath, image1, image2, fuzz):
-    print("Computing difference between", image1, "and", image2)
-    #Open first image
-    im = Image.open(basepath + "/" + image1)
-    #print(im.format, im.size, im.mode)
-
-    #Open second image
-    im2 = Image.open(basepath + "/" + image2)
-    #print(im2.format, im2.size, im2.mode)
-
-    #load the images for pixel access
-    px = im.load()
-    px2 = im2.load()
-
-    #initialize a counter for tracking number of pixels differing
-    diffpixelcount = 0
-
-    #Check if the dimensions of the images match
-    if im.size != im2.size:
-        print("Error: Size of images do not match")
-        exit()
-
-    #iterate through the pixels and check for similarity
-    for i in range(im.size[0]):
-        for j in range(im.size[1]):
-            #print(px[i,j], px2[i,j])
-            if(not comparePixels(px[i,j], px2[i,j], 255*fuzz/100)):
-                px[i,j] = (255,0,0)
-                diffpixelcount = diffpixelcount + 1
-            else:
-                px[i,j] = (math.floor(px[i,j][0]*.75),
-                           math.floor(px[i,j][1]*.75),
-                           math.floor(px[i,j][2]*.75))
-                
-    print("        Number of pixels that differ:", diffpixelcount)
-    print("        Ratio of different pixels to total pixels:", diffpixelcount/(im.size[0]*im.size[1]))
-
-    # Make diff directory
-    try:
-        os.mkdir(basepath + '/diff')
-    except FileExistsError:
-        pass # error checking l8ter
-
-    im.save(basepath + "/diff/" + image1 + " " + image2 + " - diff.jpg")
-    #diff = Image.open(basepath + "/diff/" + image1 + " " + image2 + " - diff.jpg")
-    #diff.show()
-
 def compareSubimages(basepath, image1, image2):
     im = cv2.imread(basepath + "/" + image1)
     im2 = cv2.imread(basepath + "/" + image2)
@@ -177,7 +121,6 @@ if __name__ == '__main__':
             for entry2 in os.listdir(images_path):
                 print("Entry 1:", entry, "    Entry 2:", entry2)
                 if os.path.isfile(os.path.join(images_path, entry2)) and entry != entry2 and frozenset((entry, entry2)) not in processed:
-                    #compareImages(images_path, entry, entry2, fuzz)
                     average_similarity = compareSubimages(images_path, entry, entry2)
                     print(entry, entry2, 'comparison: ', average_similarity)               
                     logFile.write(entry + ',' + entry2 + ',' + str(average_similarity) + '\n')
