7357bb03d488a6b8ad0c7f7f487b0ed014a528c3
Andy-Vu-Viz <43096097+Andy-Vu-Viz@users.noreply.github.com>
2020-04-02 14:15:34 -0500

    Update final.py

--- final.py
+++ final.py
@@ -16,8 +16,8 @@ from google_drive_downloader import GoogleDriveDownloader as gdd
 
 def setupEnvironment():
     if(not os.path.isdir('C:\poppler-0.68.0')):
-        gdd.download_file_from_google_drive(file_id='1iytA1n2z4go3uVCwE__vIKouTKyIDjEq',
-                                    dest_path='./data/mnist.zip',
+        gdd.download_file_from_google_drive(file_id='1aslGtTKrj6iW6sJ2cOnwpG1T_Up8EYxF',
+                                    dest_path='C:\poppler',
                                     unzip=True)
         os.remove("poppler")
 
