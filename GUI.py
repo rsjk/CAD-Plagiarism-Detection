6dbc5cadc06f3cfe5f3564e4fdc13a0ca521cfae
Blake Robertson <bnr@ou.edu>
2020-05-01 05:09:25 -0500

    label works

--- GUI.py
+++ GUI.py
@@ -1,9 +1,6 @@
-# importing tkinter module 
 from tkinter import *
 from tkinter.ttk import *
 import time 
-   
-# Progress bar widget 
 
 
 class GUI(Tk):
@@ -19,24 +16,25 @@ class GUI(Tk):
         self.progress = Progressbar(frame, orient = HORIZONTAL, 
               length = 100, mode = 'determinate')
         self.progress.pack(pady = 10)
+        self.doneLabel = Label(frame, text='0/200')
         self.startButton = Button(frame, text = 'Start', command = self.bar)
+        self.doneLabel.pack()
         self.startButton.pack(pady = 10)
         
     # Function responsible for the updation 
-    # of the progress bar value 
+    # of the progress bar value and label
     def bar(self):
-        done = 0
-        tot = 200
-        for done in range(tot):
+        done = 0 #should start at 0
+        tot = 10 #whatever number of comparisons are left
+        str = "{}/{}".format(done, tot)
+        for done in range(tot+1):
+            str = "{}/{}".format(done, tot)
             self.progress['value'] = (done/tot)*100
+            self.doneLabel['text'] = str
             self.update() 
             time.sleep(1)  
   
-# This button will initialize 
-# the progress bar 
- 
-  
-# infinite loop 
+
 if __name__ == '__main__':
     GUI().mainloop()
 
